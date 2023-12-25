1ee69c27436ff16813e55aece87038b5ae56c8e7
Ilya Lomonosov <lomonosov.iv@phystech.edu>
2023-12-25 16:26:00 +0400

    [ISSUE_2]

---
+++ access_to_exam/issue_2.py
@@ -0,0 +1,30 @@
+class EmojiMixin:
+    emojies = {
+        'grass': '🌿',
+        'fire': '🔥',
+        'water': '💧',
+        'electric': '⚡️',
+    }
+
+    def __str__(self):
+        return f'{self.name}/{EmojiMixin.emojies[self.poketype]}'
+
+
+class BasePokemon:
+    def __init__(self, name: str, poketype: str):
+        self.name = name
+        self.poketype = poketype
+
+    def __str__(self) -> str:
+        return f'{self.name}/{self.poketype}'
+
+
+class Pokemon(EmojiMixin, BasePokemon):
+
+    def __init__(self, name: str, poketype: str):
+        super().__init__(name, poketype)
+
+
+if __name__ == '__main__':
+    bulbasaur = Pokemon(name='Bulbasaur', poketype='grass')
+    print(bulbasaur)
